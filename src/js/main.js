484fdfe573c8ba9bb8121db3af2e7e5c61f6a5ec
KatyrevaElizaveta <li@Maksims-Mac-mini.local>
2018-11-27 15:53:28 +0500

    mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,15 @@ $(function(){
   $('.menu-btn').on('click', function(){
     $(this).toggleClass('menu-btn--close');
     $('.menu').toggleClass('menu--active');
+    $('body').toggleClass('overflow-hidden');
   });
 
   $('.menu__bg').on('click', function() {
     $(this).closest('.menu').removeClass('menu--active');
+    $('body').removeClass('overflow-hidden');
+    $('.menu-btn--close').removeClass('menu-btn--close');
   });
+
 
 
   formValidation();
